6c6c2c7ba93ea665eec272e86afa1d8d6406c7b7
Sidney233 <54933042+Sidney233@users.noreply.github.com>
2021-04-13 18:11:36 +0800

    完成顾客

--- src/main/java/DSPPCode/mapreduce/consumer_statistics/impl/ConsumerMapperImpl.java
+++ src/main/java/DSPPCode/mapreduce/consumer_statistics/impl/ConsumerMapperImpl.java
@@ -1,6 +1,8 @@
 package DSPPCode.mapreduce.consumer_statistics.impl;
 
+import DSPPCode.mapreduce.consumer_statistics.question.Consumer;
 import DSPPCode.mapreduce.consumer_statistics.question.ConsumerMapper;
+import clojure.lang.Compiler;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 
@@ -12,6 +14,11 @@ public class ConsumerMapperImpl extends ConsumerMapper {
     public void map(LongWritable key, Text value, Context context)
             throws IOException, InterruptedException {
         String[] data = value.toString().split("\t");
-        context.write(new Text(data[3]), IInteger.parseInt(data[2]));
+        boolean isvip = false;
+        if (data[3].equals("vip")) {
+            isvip = true;
+        }
+        Consumer consumer = new Consumer(data[0], Integer.parseInt(data[2]), isvip);
+        context.write(new Text(data[3]), consumer);
     }
 }
--- src/main/java/DSPPCode/mapreduce/consumer_statistics/impl/ConsumerReducerImpl.java
+++ src/main/java/DSPPCode/mapreduce/consumer_statistics/impl/ConsumerReducerImpl.java
@@ -2,15 +2,25 @@ package DSPPCode.mapreduce.consumer_statistics.impl;
 
 import DSPPCode.mapreduce.consumer_statistics.question.Consumer;
 import DSPPCode.mapreduce.consumer_statistics.question.ConsumerReducer;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 
 import java.io.IOException;
+import java.io.StringWriter;
 
 public class ConsumerReducerImpl extends ConsumerReducer {
 
     @Override
     protected void reduce(Text key, Iterable<Consumer> values, Context context)
             throws IOException, InterruptedException {
-
+        long sum = 0;
+        int count = 0;
+        for (Consumer value : values) {
+            sum += value.getMoney();
+            count += 1;
+        }
+        String output;
+        output = key + "\t" + Integer.toString(count) + "\t" + Long.toString(sum);
+        context.write(new Text(output), NullWritable.get());
     }
 }
